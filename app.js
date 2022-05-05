6f6e15ac93ad3266ab653070fc119f2c42e04168
Leo Mbm <leonidas.j.mbm@gmail.com>
2022-05-05 11:15:50 +0200

    exo1 done

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 let brussels = new Date();
 
-let usa = new Date();
-let iceland = new Date();
-let russian = new Date();
+let usa = new Date(2022, 5, 5, 12, 57).toUTCString();
+let iceland = new Date(2022, 5, 5, 8, 57).toUTCString();
+let russian = new Date(2022, 5, 5, 11, 57).toUTCString();
 
-console.log(usa.getTimezoneOffset());
+console.log("Time in Brussels :" + brussels);
+console.log("Time in Alaska :" + usa);
+console.log("Time in Iceland :" + iceland);
+console.log("Time in Russia :" + russian);
