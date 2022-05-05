e81ca9e9274f830ec45ba6afe25e1b7857b3ee0a
Leo Mbm <leonidas.j.mbm@gmail.com>
2022-05-05 12:14:42 +0200

    exo2 function

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let brussels = new Date();
 
+// Exo1
+
 let usa = new Date(2022, 5, 5, 12, 57).toUTCString();
 let iceland = new Date(2022, 5, 5, 8, 57).toUTCString();
 let russian = new Date(2022, 5, 5, 11, 57).toUTCString();
@@ -9,6 +11,8 @@ console.log("Time in Alaska :" + usa);
 console.log("Time in Iceland :" + iceland);
 console.log("Time in Russia :" + russian);
 
+// Exo2
+
 let myBirth = new Date(2002, 11, 3).getTime();
 
 let result = brussels - myBirth;
@@ -16,3 +20,21 @@ let result = brussels - myBirth;
 let numberBetween = Math.floor(result / 3600 / 24 / 1000);
 
 console.log("My Birth :" + numberBetween);
+
+// Exo 2 Function
+
+function getDaysBetween() {
+  let passed = new Date(1970, 0, 1);
+  let futur = new Date().getTime();
+
+  let result = futur - passed;
+
+  let number = Math.floor(result / 3600 / 24 / 1000);
+
+  let h1 = document.createElement("h1");
+  h1.innerHTML = "Here " + number + "days is passed since" + passed;
+
+  document.body.appendChild(h1);
+}
+
+getDaysBetween();
