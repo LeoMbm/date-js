e0c7cf92049620e168e9b6957af6ae5417539b27
Leo Mbm <leonidas.j.mbm@gmail.com>
2022-05-05 14:48:02 +0200

    exo3 finish

--- app.js
+++ app.js
@@ -24,17 +24,40 @@ console.log("My Birth :" + numberBetween);
 // Exo 2 Function
 
 function getDaysBetween() {
-  let passed = new Date(1970, 0, 1);
-  let futur = new Date().getTime();
+  let passed = new Date(1970);
+  let now = new Date().getTime();
 
-  let result = futur - passed;
+  let result = now - passed;
 
   let number = Math.floor(result / 3600 / 24 / 1000);
 
   let h1 = document.createElement("h1");
-  h1.innerHTML = "Here " + number + "days is passed since" + passed;
+  h1.innerHTML = "Here " + number + " days is passed since " + passed;
 
   document.body.appendChild(h1);
 }
 
 getDaysBetween();
+
+// Exo 3
+let div = document.createElement("div");
+div.className = "input-hours";
+let input = document.createElement("input");
+input.setAttribute("type", "number");
+input.setAttribute("placeholder", "Give me Hours");
+let pTime = document.createElement("p");
+let time = new Date();
+pTime.innerHTML = time;
+document.body.appendChild(div);
+div.appendChild(pTime);
+div.appendChild(input);
+
+input.addEventListener("keyup", calculateHours);
+
+function calculateHours() {
+  let dateNow = Date.now();
+  let value = input.value * 3600 * 1000;
+  let result = dateNow + value;
+  pTime.innerHTML = new Date(result);
+  //   console.log(result);
+}
