e2ed6e6630a81977155cddd42467870154626c32
Leo Mbm <leonidas.j.mbm@gmail.com>
2022-05-05 11:34:05 +0200

    exo2

--- app.js
+++ app.js
@@ -8,3 +8,11 @@ console.log("Time in Brussels :" + brussels);
 console.log("Time in Alaska :" + usa);
 console.log("Time in Iceland :" + iceland);
 console.log("Time in Russia :" + russian);
+
+let myBirth = new Date(2002, 11, 3).getTime();
+
+let result = brussels - myBirth;
+
+let numberBetween = Math.floor(result / 3600 / 24 / 1000);
+
+console.log("My Birth :" + numberBetween);
